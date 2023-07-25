07353ed86e5a86dd1679fce9d26b8d674bd0e382
Duygu Ciftci <cftc.duyguu@gmail.com>
2023-07-25 17:53:44 +0300

    US001_V1

---
+++ src/test/java/tests/US001/TC_001.java
@@ -0,0 +1,16 @@
+package tests.US001;
+
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+import pages.Page;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class TC_001 {
+    @Test
+    public void test01() {
+        Driver.getDriver().get(ConfigReader.getProperty("amazonURl"));
+        Page page = new Page();
+        page.aramaKutusu.sendKeys("Iphone", Keys.ENTER);
+    }
+}
---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,81 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.time.Duration;
+
+public class Driver {
+    private Driver(){
+        /*
+        Driver class'indan obje olusturmanin onune gecebilmek icin default constructor'i private yaparak
+        bunu engellemis oluruz. Bu kaliba da Singleton Pattern denir.
+         */
+    }
+    /*
+    POM(Page Object Model)
+       Test senaryolarinin daha az kod ile yazilmasina ve bakiminin daha kolay yapilmasina olanak saglayan
+       yazilim test yontemidir.TestNG ve Cucumber frameworklerinde POM kalibi kullanilir.
+     */
+     static WebDriver driver;
+
+ public static WebDriver getDriver(){
+      /*
+   Driver'i her çağırdığımızda yeni bir pencere açılmasının önüne geçmek için
+if bloğu içinde Eğer driver'a değer ATANMAMIŞSA değer ata, eğer değer atanmışsa
+driver'i aynı sayfada return et
+    */
+
+     /*
+     .properties dosyasina key olarak browser ve degerini asagida olusturdugumuz switch case'lerden birini seceriz
+     ve sectigimiz driver calisir.
+      */
+     if (driver==null) { //->Driver'a deger atanmamissa!!
+         switch (ConfigReader.getProperty("browser")){
+             case "chrome" :
+                 WebDriverManager.chromedriver().setup();
+                 driver = new ChromeDriver();
+
+                 break;
+
+             case  "edge" :
+                 WebDriverManager.edgedriver().setup();
+                 driver = new EdgeDriver();
+                 break;
+             case "safari" :
+                 WebDriverManager.safaridriver().setup();
+                 driver = new SafariDriver();
+                 break;
+             case "firefox" :
+                 WebDriverManager.firefoxdriver().setup();
+                 driver = new FirefoxDriver();
+                 break;
+             default:
+                 WebDriverManager.chromedriver().setup();
+                 driver = new ChromeDriver();
+         }
+
+         driver.manage().window().maximize();
+         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+     }
+     return driver;
+ }
+ public static void closeDriver(){
+     if(driver!=null) {//Driver'a deger atanmissa,bos degilse
+         driver.close();
+         driver=null;//Driver'i kapattiktan sonra bosalt
+     }
+
+ }
+    public static void quitDriver(){
+        if(driver!=null) {//Driver'a deger atanmissa,bos degilse
+            driver.quit();
+            driver=null;//Driver'i kapattiktan sonra bosalt
+        }
+
+    }
+}
